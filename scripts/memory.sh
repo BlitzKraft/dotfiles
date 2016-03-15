9c64fa6799cc82fbae92c86d1c58858ba27daad2
BK Bolisetty <bk@blitzkraft.me>
2016-03-15 13:08:41 -0500

    Added color and tabs.

--- scripts/memory.sh
+++ scripts/memory.sh
@@ -1,26 +1,43 @@
 #!/bin/sh
 
-FULL=▓
-EMPTY=░
+#Thanks to github.com/xero and reddit.com/u/x_ero
+
+#Born in this thread: https://www.reddit.com/r/unixporn/comments/4afp51/equivalent_of_ttyload_for_memory/
+FULL=\=
+#FULL=▓
+#EMPTY=░
+EMPTY=-
 SIZE=80
 INTERVAL=1
 
 draw() {
-  perc=$1
-  inc=$(( perc * SIZE / 100 ))
-  out="$perc% "
-  thiscolor=
-  for v in `seq 0 $(( SIZE - 1 ))`
-  do
-    test "$v" -le "$inc"   \
-    && out="${out}${FULL}" \
-    || out="${out}${EMPTY}"
-  done
-  echo $out
+	perc=$1
+	inc=$(( perc * SIZE / 100 ))
+	thiscolor=
+	nocolor="[0m"
+	case $inc in 
+	[1-5]*)
+		color="[1;32m"
+		;;
+	[6-8]*)
+		color="[36m"
+		;;
+	9[*]) color="[1;31m"
+		;;
+	esac
+	out="$perc% $color"
+	for v in `seq 0 $(( SIZE - 1 ))`
+	do
+		test "$v" -le "$inc"	 \
+		&& out="${out}${FULL}" \
+		|| out="${out}${EMPTY}"
+	done
+	out="${out}${nocolor}"
+	echo $out
 }
 
 while :; do
-  MEM=`free | awk '/Mem:/ {print int($3/$2 * 100.0)}'`
-  draw $MEM
-  sleep $INTERVAL
+	MEM=`free | awk '/Mem:/ {print int($3/$2 * 100.0)}'`
+	draw $MEM
+	sleep $INTERVAL
 done
