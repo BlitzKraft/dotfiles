d31a4953bda3fd8ea087a0cb2024011518728970
BK Bolisetty <bk@blitzkraft.me>
2016-03-17 01:54:21 -0500

    Moar colors!

--- scripts/memory.sh
+++ scripts/memory.sh
@@ -4,36 +4,61 @@
 
 #Born in this thread: https://www.reddit.com/r/unixporn/comments/4afp51/equivalent_of_ttyload_for_memory/
 FULL=\=
-#FULL=▓
-#EMPTY=░
 EMPTY=.
-#SIZE=80
-INTERVAL=30
+INTERVAL=10
+
+SIZE=$(tput cols)
+SIZE=$((SIZE - 4))
+#breakpoints in percent
+abreakpoint1=50
+abreakpoint2=80
+abreakpoint3=90
+color1="[32m"
+color2="[36m"
+color3="[33m"
+color4="[31m"
+bgcolor="[30m"
+nocolor="[0m"
 
 draw() {
 	perc=$1
-	SIZE=$(tput cols)
-	SIZE=$((SIZE * 90/100))
 	inc=$(( perc * SIZE / 100 ))
-	nocolor="[0m"
-	case $perc in 
-	[1-5]*)
-		color="[32m"
-		;;
-	[6-8]*)
-		color="[36m"
-		;;
-	[9]*) 
-		color="[31m"
-		;;
-	esac
+	breakpoint1=$(( abreakpoint1 * SIZE / 100))
+	breakpoint2=$(( abreakpoint2 * SIZE / 100))
+	breakpoint3=$(( abreakpoint3 * SIZE / 100))
+#	case $perc in 
+#	[1-5]*)
+#		color="[32m"
+#		;;
+#	[6-8]*)
+#		color="[36m"
+#		;;
+#	[9]*) 
+#		color="[31m"
+#		;;
+#	esac
 	out="$perc% "
 	out="$out $color"
 	for v in `seq 0 $(( SIZE - 1 ))`
 	do
+		if (( v <= breakpoint1 ))
+		then
+			color=$color1
+		elif (( v > breakpoint1 && v <= breakpoint2 ))
+		then
+			color=$color2
+		elif (( v > breakpoint2 && v <= breakpoint3 ))
+		then
+			color=$color3
+		elif (( v > breakpoint3 ))
+		then 
+			color=$color4
+		fi
+
 		test "$v" -le "$inc"	 \
-		&& out="${out}${FULL}" \
-		|| out="${out}${EMPTY}"
+		&& out="${out}${color}${FULL}${nocolor}" \
+		|| out="${out}${bgcolor}${EMPTY}${nocolor}"
+		#|| out="${out}${color4}${EMPTY}${nocolor}"
 	done
 	out="${out}${nocolor}"
 	echo $out
