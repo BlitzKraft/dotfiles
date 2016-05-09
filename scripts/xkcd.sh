6121b6bbbf6147a2e9d5dbf9dffa521fa61fe32d
BK Bolisetty <bk@blitzkraft.me>
2016-05-09 15:48:43 -0500

    It runs from my fork instead of installed version.

--- scripts/xkcd.sh
+++ scripts/xkcd.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 cd /home/bk
-/usr/local/bin/xkcd-dl --update-db  > /dev/null
-/usr/local/bin/xkcd-dl --download-all > /dev/null
+#/usr/local/bin/xkcd-dl --update-db  > /dev/null
+#/usr/local/bin/xkcd-dl --download-all > /dev/null
+/home/bk/Documents/xkcd-dl/xkcd_dl/cli.py  --update-db > /dev/null
+/home/bk/Documents/xkcd-dl/xkcd_dl/cli.py  --download-all > /dev/null
 count=$(/bin/ls /home/bk/xkcd_archive/ | /usr/bin/wc -l)
 /bin/echo "Updated database"
 /bin/echo "Currently $count comics in db"
